c5c3ae392219bc4930031f4300d03b5ae77a350b
DESKTOP-A8DOM8H\Markku Leskinen <mtlesk@gmail.com>
2020-04-25 20:44:26 +0300

    Add city name to results

--- foursquare/foursquare.py
+++ foursquare/foursquare.py
@@ -1,3 +1,5 @@
+import time
+import numpy as np
 import requests
 import pandas as pd
 
@@ -29,11 +31,13 @@ class Venues(Settings, Credentials):
         super().__init__(client_id, client_secret)
         self.venue_df = pd.DataFrame()
 
-    def get_nearby_venues(self, names, latitudes, longitudes):
+    def get_nearby_venues(self, city_list, latitudes, longitudes):
+
+        print("Matching biergartens in " + city_list[0] + ' to Foursquare venues...')
 
         venues_list = []
 
-        for lat, lng, name in zip(latitudes, longitudes, names):
+        for city, lat, lng in zip(city_list, latitudes, longitudes):
 
             # create the API request URL
             url = 'https://api.foursquare.com/v2/venues/explore?&client_id={}&client_secret={}&v={}&ll={},{} \
@@ -52,7 +56,7 @@ class Venues(Settings, Credentials):
 
             # return only relevant information for each nearby venue
             venues_list.append([(
-                name,
+                city,
                 lat,
                 lng,
                 v['venue']['id'],
@@ -61,10 +65,13 @@ class Venues(Settings, Credentials):
                 v['venue']['location']['lng'],
                 v['venue']['categories'][0]['name']) for v in results])
 
+            # Sleep long enough to ensure compliance with Foursquare API guidelines
+            time.sleep(0.51)
+
         nearby_venues = pd.DataFrame([item for venue_list in venues_list for item in venue_list])
-        nearby_venues.columns = ['Name',
-                                 'Name Latitude',
-                                 'Name Longitude',
+        nearby_venues.columns = ['City',
+                                 'OSM Latitude',
+                                 'OSM Longitude',
                                  'venue_id',
                                  'Venue',
                                  'Venue Latitude',
@@ -78,23 +85,38 @@ class Reviews(Settings, Credentials):
 
     def __init__(self, client_id, client_secret):
         super().__init__(client_id, client_secret)
+        self.review_df = None
 
-    def get_venue_review(self, venue_id):
-        url = 'https://api.foursquare.com/v2/venues/{}?client_id={}&client_secret={}&v={}'.format(
-            venue_id,
-            self.client_id,
-            self.client_secret,
-            Settings.version)
+    def get_venue_review(self, city_list, venue_id_list):
 
-        response = requests.get(url).json()
+        print("Fetching reviews for identified biergartens in " + city_list[0] + '...')
 
         res_df = pd.DataFrame(columns=['venue_id', 'venue_name', 'rating', 'likes_cnt'])
-        d = {
-            'venue_id': venue_id,
-            'venue_name': response['response']['venue']['name'],
-            'rating': response['response']['venue']['rating'],
-            'likes_cnt': response['response']['venue']['likes']['count']
-        }
-        res_df = res_df.append(d, ignore_index=True)
 
-        return res_df
+        for city, venue_id in zip(city_list, venue_id_list):
+
+            url = 'https://api.foursquare.com/v2/venues/{}?client_id={}&client_secret={}&v={}'.format(
+                venue_id,
+                self.client_id,
+                self.client_secret,
+                Settings.version)
+
+            response = requests.get(url).json()
+
+            d = {
+                'city': city,
+                'venue_id': venue_id,
+                'venue_name': response['response']['venue'].get('name'),
+                'rating': response['response']['venue'].get('rating'),
+            }
+
+            if response['response']['venue']['likes']:
+                d['likes_cnt'] = response['response']['venue']['likes'].get('count')
+            else:
+                d['likes_cnt'] = np.NaN
+
+            res_df = res_df.append(d, ignore_index=True)
+
+        self.review_df = res_df
+
+        return self.review_df
